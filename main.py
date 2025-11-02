3ebf4dff9c1eab2532849b0399923b1f4f2210d3
Daniel Graef <82603437+14-dg@users.noreply.github.com>
2025-11-02 19:31:12 +0100

    changed main.py to the intended structure for the project

--- main.py
+++ main.py
@@ -1,15 +1,22 @@
 
+from constants import *
 from cell import Cell
 from grid import Grid
 from pathfinder import Pathfinder
+from drawing_grid import DrawingGrid
 from drawing_board import DrawingBoard
 
 
 
 
 def main():
-    g = Grid(70, 40, 20, 2)
-    db = DrawingBoard(g)
+    g1 = Grid(35, 40, grid_name=MAIN_GRID)
+    g2 = Grid(35, 40, grid_name=SECONDARY_GRID)
+    
+    dg1 = DrawingGrid(g1, 20, 2, 0, 0)
+    dg2 = DrawingGrid(g2, 20, 2, dg1.get_screen_dimensions()[0], 0)
+    
+    db = DrawingBoard(dg1=dg1, dg2=dg2)
     db.mainloop()
 
 if __name__ == "__main__":
