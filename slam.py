8b61997bbe2fdbed1dd89b7cad2dd7c63dea6419
Daniel Graef <82603437+14-dg@users.noreply.github.com>
2025-11-06 19:39:26 +0100

    added a slam.py with a Slam class. this is where the logic for the slam algorithm is supposed to go

---
+++ slam.py
@@ -0,0 +1,10 @@
+
+from grid import Grid
+from cell import Cell
+
+class Slam:
+    def __init__(self, grid: Grid) -> None:
+        self.grid = grid
+        
+    def foo(self) -> None:
+        print("Slam foo method called.")
